481753efc28cca3acc98624f4f0e5d9874335cd0
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-20 10:00:55 -0800

    st

--- lib/src/view/homeview/HomeView.dart
+++ lib/src/view/homeview/HomeView.dart
@@ -2,6 +2,7 @@ import 'package:air_bnb/src/controller/assets/appImages/appImages.dart';
 import 'package:air_bnb/src/controller/components/primaryText.dart';
 import 'package:air_bnb/src/controller/components/tabBar.dart';
 import 'package:air_bnb/src/controller/constants/colors/appColors.dart';
+import 'package:air_bnb/src/view/homeview/home-view-widgets/tab-row.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -121,26 +122,10 @@ class _HomeviewState extends State<Homeview> {
               ],
             ),
           ),
+
           SizedBox(height: 20.h),
           // Tab bar
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                CustomTabBar(icon: Icons.bug_report_outlined, text: 'OMG!'),
-                CustomTabBar(icon: Icons.stars, text: 'Icons'),
-                CustomTabBar(
-                    icon: Icons.trending_up_outlined, text: 'Trending'),
-                CustomTabBar(
-                    icon: Icons.home_work_outlined, text: 'Earth homes'),
-                CustomTabBar(icon: Icons.wind_power, text: 'Windmills'),
-                CustomTabBar(icon: Icons.vpn_key_outlined, text: 'New'),
-                CustomTabBar(
-                    icon: Icons.location_city_rounded, text: 'Top cities'),
-              ],
-            ),
-          ),
-
+          TabRowWidget(),
           SizedBox(
             height: 10.h,
           ),
