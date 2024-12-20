e94fac4d13704f8743a1b00a3f34ca5062d52ef3
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-20 10:06:15 -0800

    up

--- lib/src/view/homeview/HomeView.dart
+++ lib/src/view/homeview/HomeView.dart
@@ -6,6 +6,8 @@ import 'package:air_bnb/src/view/homeview/home-view-widgets/tab-row.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'home-view-widgets/costum-search.dart';
+
 class Homeview extends StatefulWidget {
   Homeview({super.key});
 
@@ -32,97 +34,7 @@ class _HomeviewState extends State<Homeview> {
         children: [
           SizedBox(height: 35.h),
           // Search bar and filter
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isFocused = true; // Focus enable
-                      });
-                    },
-                    child: Container(
-                      height: 40.h,
-                      decoration: BoxDecoration(
-                        color: appColors.whiteBG,
-                        borderRadius: BorderRadius.circular(30),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 4,
-                            color: appColors.grey,
-                          )
-                        ],
-                      ),
-                      child: isFocused
-                          ? TextField(
-                              autofocus: true,
-                              onSubmitted: (value) {
-                                // Handle submission
-                                setState(() {
-                                  isFocused =
-                                      false; // Dismiss focus after submission
-                                });
-                              },
-                              decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: 'Search your destination...',
-                                contentPadding:
-                                    const EdgeInsets.symmetric(horizontal: 10),
-                                icon: Padding(
-                                  padding: const EdgeInsets.only(left: 10),
-                                  child: Icon(Icons.search,
-                                      size: 24, color: appColors.grey),
-                                ),
-                              ),
-                            )
-                          : Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 10),
-                              child: Row(
-                                children: [
-                                  Icon(Icons.search,
-                                      size: 24, color: appColors.grey),
-                                  SizedBox(width: 10.w),
-                                  Expanded(
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Primarytext(
-                                          text: 'Where to?',
-                                          fontSize: 12.sp,
-                                          fontWeight: FontWeight.w500,
-                                          textColor: appColors.blacktext,
-                                        ),
-                                        Primarytext(
-                                          text: 'Anywhere Any week Add guests',
-                                          fontSize: 10.sp,
-                                          fontWeight: FontWeight.w300,
-                                          textColor: appColors.grey,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10.w),
-                CircleAvatar(
-                  backgroundColor: appColors.whiteBG,
-                  radius: 20.r,
-                  child: Icon(Icons.filter_alt, color: appColors.grey),
-                ),
-              ],
-            ),
-          ),
-
+          CustomSearchBar(),
           SizedBox(height: 20.h),
           // Tab bar
           TabRowWidget(),
