5986d4ee26468c1edc0fdf545941adec2bdf8f10
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-20 22:18:33 -0800

    up

--- lib/src/view/homeview/navbar-home.dart
+++ lib/src/view/homeview/navbar-home.dart
@@ -1,10 +1,10 @@
+import 'package:air_bnb/src/view/homeview/explore-view/HomeView.dart';
 import 'package:air_bnb/src/view/homeview/mesages-view/messagesView.dart';
 import 'package:air_bnb/src/view/homeview/profile-view/profileView.dart';
 import 'package:air_bnb/src/view/homeview/trips-view/tripsView.dart';
 import 'package:air_bnb/src/view/homeview/wishlist-view/wishListView.dart';
 import 'package:flutter/material.dart';
 import 'package:air_bnb/src/controller/constants/colors/appColors.dart';
-import 'homeView.dart'; // Replace with the correct path to the HomeView file.
 
 class MainHomeView extends StatefulWidget {
   const MainHomeView({Key? key}) : super(key: key);
