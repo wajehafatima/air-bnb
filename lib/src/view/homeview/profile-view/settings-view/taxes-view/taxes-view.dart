a114550938c6d50716ff9012f37631505734af89
Wajeha Fatima <maharfatima90@gmail.com>
2024-12-23 00:21:32 -0800

    update

---
+++ lib/src/view/homeview/profile-view/settings-view/taxes-view/taxes-view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class taxesView extends StatefulWidget {
+  const taxesView({super.key});
+
+  @override
+  State<taxesView> createState() => _taxesViewState();
+}
+
+class _taxesViewState extends State<taxesView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
