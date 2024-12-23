d3128c4f78825e48eaf71e2f9ac4ad520d619805
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-23 09:49:37 -0800

    update code

--- lib/src/view/homeview/profile-view/settings-view/taxes-view/taxes-view.dart
+++ lib/src/view/homeview/profile-view/settings-view/taxes-view/taxes-view.dart
@@ -1,1 +1,63 @@
+import 'package:flutter/material.dart';
 
+class TaxesView extends StatefulWidget {
+  const TaxesView({super.key});
+
+  @override
+  State<TaxesView> createState() => _TaxesViewState();
+}
+
+class _TaxesViewState extends State<TaxesView>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this); // Two tabs
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Taxes View'),
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.red,
+          labelColor: Colors.red,
+          unselectedLabelColor: Colors.grey,
+          tabs: const [
+            Tab(text: 'Tax Payers'),
+            Tab(text: 'Documents'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          _buildTaxPayersTab(),
+          _buildDocumentsTab(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTaxPayersTab() {
+    return const Center(
+      child: Text('Tax Payers Content'),
+    );
+  }
+
+  Widget _buildDocumentsTab() {
+    return const Center(
+      child: Text('Documents Content'),
+    );
+  }
+}
