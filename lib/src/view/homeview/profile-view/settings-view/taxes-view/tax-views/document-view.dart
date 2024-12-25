cb99e5c81057a1f54c864a63a41f0a73e0c6afd6
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-24 20:44:24 -0800

    update code

--- lib/src/view/homeview/profile-view/settings-view/taxes-view/tax-views/document-view.dart
+++ lib/src/view/homeview/profile-view/settings-view/taxes-view/tax-views/document-view.dart
@@ -16,33 +16,50 @@ class _DocumentsScreenState extends State<DocumentsScreen> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20,right: 20),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children: [SizedBox(height:20.h),
-        Primarytext(text: 'Tax documents required for filling taxes are available to review and download here.', fontSize: 10.sp, fontWeight:FontWeight.w300,
-            textColor: appColors.blacktext),
-        SizedBox(height: 10.h,),
-        Row(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Primarytext(text: 'you can also file taxes using detailed earning info, available in the',
-                fontSize: 10.sp, fontWeight:FontWeight.w300, textColor: appColors.blacktext),
-
-
-
-        UnderlinedText(text: 'earning ceremony.', onPressed: (){}),]),
-        SizedBox(height: 10.h,),
-        Divider(),
-        SizedBox(height: 15.h,),
-        TaxdocsWidget(Text1: '2024', Text2: 'No tax documnets issued', onTap: (){}),
-          TaxdocsWidget(Text1: '2023', Text2: 'No tax documnets issued', onTap: (){}),
-          TaxdocsWidget(Text1: '2022', Text2: 'No tax documnets issued', onTap: (){}),
-          TaxdocsWidget(Text1: '2021', Text2: 'No tax documnets issued', onTap: (){}),
-          TaxdocsWidget(Text1: '2020', Text2: 'No tax documnets issued', onTap: (){}),
-
-
-
-
-      ],),
+            SizedBox(height: 20.h),
+            Primarytext(
+                text:
+                    'Tax documents required for filling taxes are available to review and download here.',
+                fontSize: 10.sp,
+                fontWeight: FontWeight.w300,
+                textColor: appColors.blacktext),
+            SizedBox(
+              height: 10.h,
+            ),
+            Row(children: [
+              Primarytext(
+                  text:
+                      'you can also file taxes using detailed earning info\navailable in the',
+                  fontSize: 10.sp,
+                  fontWeight: FontWeight.w300,
+                  textColor: appColors.blacktext),
+              // UnderlinedText(text: 'earning ceremony.', onPressed: () {}),
+            ]),
+            SizedBox(
+              height: 10.h,
+            ),
+            Divider(),
+            SizedBox(
+              height: 15.h,
+            ),
+            TaxdocsWidget(
+                Text1: '2024', Text2: 'No tax documnets issued', onTap: () {}),
+            TaxdocsWidget(
+                Text1: '2023', Text2: 'No tax documnets issued', onTap: () {}),
+            TaxdocsWidget(
+                Text1: '2022', Text2: 'No tax documnets issued', onTap: () {}),
+            TaxdocsWidget(
+                Text1: '2021', Text2: 'No tax documnets issued', onTap: () {}),
+            TaxdocsWidget(
+                Text1: '2020', Text2: 'No tax documnets issued', onTap: () {}),
+          ],
+        ),
+      ),
     );
   }
 }
