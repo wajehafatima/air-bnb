242157ec3d818e2e22de9b80332bc65c9cc290af
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-24 20:42:53 -0800

    up

--- lib/src/view/homeview/profile-view/settings-view/taxes-view/tax-views/taxpayers-view.dart
+++ lib/src/view/homeview/profile-view/settings-view/taxes-view/tax-views/taxpayers-view.dart
@@ -17,44 +17,80 @@ class _TaxPayersScreenState extends State<TaxPayersScreen> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20,right: 20),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children: [SizedBox(height: 20.h,),
-          Primarytext(text: 'Taxpayer information', fontSize: 20.sp, fontWeight: FontWeight.w500,
-              textColor:appColors.blacktext),
-
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 20.h,
+          ),
+          Primarytext(
+              text: 'Taxpayer information',
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w500,
+              textColor: appColors.blacktext),
+          Row(children: [
+            Primarytext(
+              text: 'Tax information is required for most countries/regions.',
+              fontSize: 10.sp,
+              fontWeight: FontWeight.w300,
+              textColor: appColors.blacktext,
+            ),
+            //   UnderlinedText(text: 'Learn more', onPressed: () {}),
+          ]),
+          SizedBox(height: 10.h),
+          Custombutton(
+            text: 'Add tax info',
+            onTap: () {},
+            color: appColors.blacktext,
+            textColor: appColors.whiteBG,
+            width: 130.w,
+          ),
+          SizedBox(height: 30.h),
+          Primarytext(
+              text: 'Value Added Tax(VAT)',
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w500,
+              textColor: appColors.blacktext),
+          Row(children: [
+            Primarytext(
+                text: 'If you are VAT-registered, please add your VAT ID.',
+                fontSize: 10.sp,
+                fontWeight: FontWeight.w300,
+                textColor: appColors.blacktext),
+            // UnderlinedText(text: 'Learn more', onPressed: () {}),
+          ]),
+          SizedBox(
+            height: 10.h,
+          ),
+          Custombutton(
+            text: 'Add VAT ID number',
+            onTap: () {},
+            color: appColors.blacktext,
+            textColor: appColors.whiteBG,
+            width: 180.w,
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          Primarytext(
+              text: 'Need help?',
+              fontSize: 20.sp,
+              fontWeight: FontWeight.w500,
+              textColor: appColors.blacktext),
           Row(
             children: [
-              Primarytext(text: 'Tax information is required for most countries/regions.', fontSize:10.sp,
-                  fontWeight:FontWeight.w300, textColor:appColors.blacktext,),
-              UnderlinedText(text: 'Learn more', onPressed: (){}),]),
-              SizedBox(height:10.h),
-              Custombutton(text: 'Add tax info', onTap: (){},color: appColors.blacktext,textColor: appColors.whiteBG,width: 100.w,),
-              SizedBox(height:30.h),
-              Primarytext(text: 'Value Added Tax(VAT)', fontSize:20.sp, fontWeight:FontWeight.w500, textColor:appColors.blacktext),
-              Row(children: [
-                Primarytext(text: 'If you are VAT-registered, please add your VAT ID.', fontSize: 10.sp, fontWeight: FontWeight.w300, textColor: appColors.blacktext),
-                UnderlinedText(text: 'Learn more', onPressed:(){}),]),
-
-              SizedBox(height: 10.h,),
-
-              Custombutton(text: 'Add VAT ID number', onTap: (){},color: appColors.blacktext,textColor:
-              appColors.whiteBG,width: 140.w,),
-            SizedBox(height: 20.h,),
-              Primarytext(text: 'Need help?', fontSize:20.sp, fontWeight:FontWeight.w500, textColor:appColors.blacktext),
-
-              Row(children: [
-                Primarytext(text: 'Get answers to questions about taxes in our.', fontSize: 10.sp, fontWeight: FontWeight.w300, textColor: appColors.blacktext),
-                UnderlinedText(text: 'Help Center.', onPressed: () {  },)
-              ],),
-
-
-
-
-
-
-
-
+              Primarytext(
+                  text: 'Get answers to questions about taxes in our.',
+                  fontSize: 10.sp,
+                  fontWeight: FontWeight.w300,
+                  textColor: appColors.blacktext),
+              UnderlinedText(
+                text: 'Help Center.',
+                onPressed: () {},
+              )
+            ],
+          ),
         ],
       ),
     );
