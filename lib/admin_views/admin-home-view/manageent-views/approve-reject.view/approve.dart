59961e22492c30b8d0d4434e430689d1cdb15eee
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2024-12-28 07:48:09 -0800

    update code

---
+++ lib/admin_views/admin-home-view/manageent-views/approve-reject.view/approve.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
