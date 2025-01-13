535d2f6dbe1751e6e8867c2e6071cf653087ff9f
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2025-01-13 09:25:46 +0500

    up

--- lib/src/controller/assets/appImages/appImages.dart
+++ lib/src/controller/assets/appImages/appImages.dart
@@ -5,6 +5,6 @@ class appImages {
   static String hotel2= 'lib/src/controller/assets/appImages/hotel2.jpg';
   static String hotel3= 'lib/src/controller/assets/appImages/hotel3.jpg';
   static String hotel4= 'lib/src/controller/assets/appImages/hotel4.jpg';
-  static String dp= 'lib/src/controller/assets/appImages/dp.jpg';
+  static String dp= 'assets/splash.jpg';//'lib/src/controller/assets/appImages/dp.jpg';
   static String hosting= 'lib/src/controller/assets/appImages/hosting.jpg';
 }
