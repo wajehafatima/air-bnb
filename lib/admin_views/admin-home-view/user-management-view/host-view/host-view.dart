d070832b5d88a35310f227d4af3a9ff09ca83595
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2025-01-13 11:02:29 +0500

    up

--- lib/admin_views/admin-home-view/user-management-view/host-view/host-view.dart
+++ lib/admin_views/admin-home-view/user-management-view/host-view/host-view.dart
@@ -53,8 +53,6 @@
 //     );
 //   }
 // }
-import 'package:air_bnb/src/controller/components/primaryText.dart';
-import 'package:air_bnb/src/controller/constants/colors/appColors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -63,49 +61,67 @@ class HostView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Expanded sample data with more rows
+    final List<List<String>> data = [
+      ['ID', 'Host Name', 'Listing Name', 'Number of Reviews', 'Price', 'Total Listings', 'Bookings', 'Earnings'],
+      ['1', 'Host 1', 'Listing 1', '5', '\$100', '1', '10', '\$500'],
+      ['2', 'Host 2', 'Listing 2', '10', '\$200', '2', '20', '\$1000'],
+      ['3', 'Host 3', 'Listing 3', '15', '\$300', '3', '30', '\$1500'],
+      ['4', 'Host 4', 'Listing 4', '20', '\$400', '4', '40', '\$2000'],
+      ['5', 'Host 5', 'Listing 5', '25', '\$500', '5', '50', '\$2500'],
+      ['6', 'Host 6', 'Listing 6', '30', '\$600', '6', '60', '\$3000'],
+      ['7', 'Host 7', 'Listing 7', '35', '\$700', '7', '70', '\$3500'],
+      ['8', 'Host 8', 'Listing 8', '40', '\$800', '8', '80', '\$4000'],
+      ['9', 'Host 9', 'Listing 9', '45', '\$900', '9', '90', '\$4500'],
+      ['10', 'Host 10', 'Listing 10', '50', '\$1000', '10', '100', '\$5000'],
+      ['11', 'Host 11', 'Listing 11', '55', '\$1100', '11', '110', '\$5500'],
+      ['12', 'Host 12', 'Listing 12', '60', '\$1200', '12', '120', '\$6000'],
+      ['13', 'Host 13', 'Listing 13', '65', '\$1300', '13', '130', '\$6500'],
+      ['14', 'Host 14', 'Listing 14', '70', '\$1400', '14', '140', '\$7000'],
+      ['15', 'Host 15', 'Listing 15', '75', '\$1500', '15', '150', '\$7500'],
+    ];
+
+    // Transpose the data (convert rows to columns and vice versa)
+    final transposedData = _transpose(data);
+
     return Padding(
-        padding: EdgeInsets.all(16.0.w),
+      padding: EdgeInsets.all(16.0.w),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal, // Scroll horizontally for wide tables
         child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal, // Allows horizontal scrolling for wide tables
-          child: DataTable(
-            columnSpacing: 16.w, // Adjust spacing between columns
-            headingRowColor: MaterialStateProperty.resolveWith(
-                    (states) => appColors.secondary),
-            border: TableBorder(
-              horizontalInside: BorderSide(color: appColors.grey, width: 0.5),
-            ),
-            columns: const [
-              DataColumn(label: Text('ID',)),
-              DataColumn(label: Text('Host Name')),
-              DataColumn(label: Text('Listing Name')),
-              DataColumn(label: Text('Number of Reviews')),
-              DataColumn(label: Text('Price')),
-              DataColumn(label: Text('Total Listings')),
-              DataColumn(label: Text('Bookings')),
-              DataColumn(label: Text('Earnings')),
-            ],
-            rows: _generateTableRows(),
+          scrollDirection: Axis.vertical, // Scroll vertically for tall tables
+          child: Table(
+            border: TableBorder.all(color: Colors.grey, width: 0.5),
+            columnWidths: const <int, TableColumnWidth>{
+              0: FixedColumnWidth(120), // Fix the first column width
+            },
+            defaultColumnWidth: IntrinsicColumnWidth(), // Adjust column width to content
+            children: transposedData.map((row) {
+              return TableRow(
+                children: row.map((cell) {
+                  return Padding(
+                    padding: EdgeInsets.all(8.0.w),
+                    child: Text(
+                      cell,
+                      style: TextStyle(fontSize: 14.sp),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                }).toList(),
+              );
+            }).toList(),
           ),
         ),
-
+      ),
     );
   }
 
-  // Function to generate table rows dynamically
-  List<DataRow> _generateTableRows() {
-    return List.generate(10, (index) {
-      return DataRow(
-        cells: [
-          DataCell(Text('ID: $index')),
-          DataCell(Text('Host Name $index')),
-          DataCell(Text('Listing $index')),
-          DataCell(Text('${(index + 1) * 5} Reviews')),
-          DataCell(Text('\$${(index + 1) * 100}')),
-          DataCell(Text('${(index + 1)} Listings')),
-          DataCell(Text('${(index + 2) * 10} Bookings')),
-          DataCell(Text('\$${(index + 1) * 500}')),
-        ],
-      );
-    });
+  /// Transpose function: Converts rows to columns and columns to rows
+  List<List<String>> _transpose(List<List<String>> data) {
+    return List.generate(
+      data[0].length,
+          (i) => List.generate(data.length, (j) => data[j][i]),
+    );
   }
 }
+  
