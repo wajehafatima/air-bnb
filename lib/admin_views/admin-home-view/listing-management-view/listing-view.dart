d33d9a3cac0624b204ed70d3afacb942f0488530
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2025-01-12 18:43:37 -0800

    bb

--- lib/admin_views/admin-home-view/listing-management-view/listing-view.dart
+++ lib/admin_views/admin-home-view/listing-management-view/listing-view.dart
@@ -1,3 +1,189 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// import '../../../src/controller/components/primaryText.dart';
+// import '../../../src/controller/constants/colors/appColors.dart';
+
+// class Listingview extends StatefulWidget {
+//   const Listingview({super.key});
+
+//   @override
+//   State<Listingview> createState() => _ListingviewState();
+// }
+
+// class _ListingviewState extends State<Listingview>
+//     with SingleTickerProviderStateMixin {
+//   late TabController _tabController;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _tabController = TabController(length: 2, vsync: this);
+//   }
+
+//   @override
+//   void dispose() {
+//     _tabController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: appColors.whiteBG,
+//       appBar: AppBar(
+//         backgroundColor: appColors.whiteBG,
+//         title: Primarytext(
+//           text: 'Listing Management',
+//           fontSize: 25.sp,
+//           fontWeight: FontWeight.w500,
+//           textColor: appColors.blacktext,
+//         ),
+//         bottom: TabBar(
+//           controller: _tabController,
+//           labelColor: appColors.blacktext,
+//           indicatorColor: appColors.secondary,
+//           unselectedLabelColor: appColors.grey,
+//           tabs: [
+//             Tab(text: "Manage Listings"),
+//             Tab(text: "Control Ranking"),
+//           ],
+//         ),
+//       ),
+//       body: TabBarView(
+//         controller: _tabController,
+//         children: [
+//           ManageListingsWidget(),
+//           ControlRankingWidget(),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class ManageListingsWidget extends StatelessWidget {
+//   const ManageListingsWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       padding: EdgeInsets.all(16.w),
+//       itemCount: 10,
+//       itemBuilder: (context, index) {
+//         return Card(
+//           color: appColors.whiteBG,
+//           elevation: 5,
+//           margin: EdgeInsets.only(bottom: 16.h),
+//           shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.circular(12.r),
+//           ),
+//           child: Padding(
+//             padding: EdgeInsets.all(16.w),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Primarytext(
+//                   text: "Listing #${index + 1}",
+//                   fontSize: 18.sp,
+//                   fontWeight: FontWeight.w600,
+//                   textColor: appColors.blacktext,
+//                 ),
+//                 SizedBox(height: 8.h),
+//                 Primarytext(
+//                   text: "Status: Pending Approval",
+//                   fontSize: 14.sp,
+//                   textColor: appColors.grey,
+//                   fontWeight: FontWeight.w400,
+//                 ),
+//                 SizedBox(height: 8.h),
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     ElevatedButton(
+//                         onPressed: () {},
+//                         style: ElevatedButton.styleFrom(
+//                           backgroundColor: appColors.whiteBG,
+//                         ),
+//                         child: Primarytext(
+//                             text: 'Approve',
+//                             fontSize: 11.sp,
+//                             fontWeight: FontWeight.w600,
+//                             textColor: appColors.blacktext)),
+//                     ElevatedButton(
+//                         onPressed: () {},
+//                         style: ElevatedButton.styleFrom(
+//                           backgroundColor: appColors.whiteBG,
+//                         ),
+//                         child: Primarytext(
+//                             text: 'Reject',
+//                             fontSize: 11.sp,
+//                             fontWeight: FontWeight.w600,
+//                             textColor: appColors.secondary)),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+
+// class ControlRankingWidget extends StatelessWidget {
+//   const ControlRankingWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView.builder(
+//       padding: EdgeInsets.all(16.w),
+//       itemCount: 10,
+//       itemBuilder: (context, index) {
+//         return Card(
+//           color: appColors.whiteBG,
+//           elevation: 5,
+//           margin: EdgeInsets.only(bottom: 16.h),
+//           shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.circular(12.r),
+//           ),
+//           child: Padding(
+//             padding: EdgeInsets.all(16.w),
+//             child: Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 Primarytext(
+//                   text: "Listing #${index + 1}",
+//                   fontSize: 16.sp,
+//                   fontWeight: FontWeight.w500,
+//                   textColor: appColors.blacktext,
+//                 ),
+//                 Row(
+//                   children: [
+//                     IconButton(
+//                       onPressed: () {},
+//                       icon: Icon(
+//                         Icons.arrow_upward,
+//                         color: appColors.grey,
+//                       ),
+//                     ),
+//                     IconButton(
+//                       onPressed: () {},
+//                       icon: Icon(
+//                         Icons.arrow_downward,
+//                         color: appColors.secondary,
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+import 'package:air_bnb/admin_views/admin-home-view/manageent-views/managementView.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -45,8 +231,20 @@ class _ListingviewState extends State<Listingview>
           indicatorColor: appColors.secondary,
           unselectedLabelColor: appColors.grey,
           tabs: [
-            Tab(text: "Manage Listings"),
-            Tab(text: "Control Ranking"),
+            Tab(
+              child: Primarytext(
+                  text: 'Manage listing',
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.w500,
+                  textColor: appColors.grey),
+            ),
+            Tab(
+              child: Primarytext(
+                  text: 'Control Ranking',
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.w500,
+                  textColor: appColors.grey),
+            ),
           ],
         ),
       ),
@@ -70,55 +268,85 @@ class ManageListingsWidget extends StatelessWidget {
       padding: EdgeInsets.all(16.w),
       itemCount: 10,
       itemBuilder: (context, index) {
-        return Card(
-          color: appColors.whiteBG,
-          elevation: 5,
-          margin: EdgeInsets.only(bottom: 16.h),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.r),
+        return Container(
+          // height: 160.h,
+          width: 340.w,
+          decoration: BoxDecoration(
+            color: appColors.whiteBG,
+            border: Border.all(color: appColors.grey),
+            borderRadius: BorderRadius.circular(10),
           ),
+          margin: EdgeInsets.only(bottom: 10.h),
           child: Padding(
-            padding: EdgeInsets.all(16.w),
+            padding: EdgeInsets.only(left: 16.w, right: 16.w, top: 16.h),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Primarytext(
                   text: "Listing #${index + 1}",
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.w600,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w400,
                   textColor: appColors.blacktext,
                 ),
-                SizedBox(height: 8.h),
+                SizedBox(height: 5.h),
+                Primarytext(
+                    text: 'Host ID: 1',
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w300,
+                    textColor: AppColors.blackText),
+                SizedBox(
+                  height: 5.h,
+                ),
+                Primarytext(
+                    text: 'Host name: John',
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w300,
+                    textColor: AppColors.blackText),
+                SizedBox(height: 5.h),
                 Primarytext(
                   text: "Status: Pending Approval",
                   fontSize: 14.sp,
                   textColor: appColors.grey,
                   fontWeight: FontWeight.w400,
                 ),
-                SizedBox(height: 8.h),
+                SizedBox(height: 5.h),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: appColors.whiteBG,
-                        ),
-                        child: Primarytext(
-                            text: 'Approve',
-                            fontSize: 11.sp,
-                            fontWeight: FontWeight.w600,
-                            textColor: appColors.blacktext)),
-                    ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: appColors.whiteBG,
-                        ),
-                        child: Primarytext(
-                            text: 'Reject',
-                            fontSize: 11.sp,
-                            fontWeight: FontWeight.w600,
-                            textColor: appColors.secondary)),
+                    Expanded(
+                      child: Container(
+                          height: 30.h,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: appColors.whiteBG,
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: appColors.grey)),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Primarytext(
+                                text: 'Approve',
+                                fontSize: 12.sp,
+                                fontWeight: FontWeight.w600,
+                                textColor: appColors.blacktext),
+                          )),
+                    ),
+                    Expanded(
+                      child: Container(
+                          height: 30.h,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: appColors.whiteBG,
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: appColors.grey)),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Primarytext(
+                                text: 'Reject',
+                                fontSize: 12.sp,
+                                fontWeight: FontWeight.w600,
+                                textColor: appColors.blacktext),
+                          )),
+                    ),
                   ],
                 ),
               ],
@@ -154,7 +382,7 @@ class ControlRankingWidget extends StatelessWidget {
                 Primarytext(
                   text: "Listing #${index + 1}",
                   fontSize: 16.sp,
-                  fontWeight: FontWeight.w500,
+                  fontWeight: FontWeight.w300,
                   textColor: appColors.blacktext,
                 ),
                 Row(
