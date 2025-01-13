2de601727e90e7079736a3ee9e0cce8c30298864
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2025-01-12 18:49:26 -0800

    up

--- lib/main.dart
+++ lib/main.dart
@@ -63,10 +63,9 @@ class MyApp extends StatelessWidget {
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
             ),
-            UserManagementView()
-            //Editprofileview()
 
-            // UserManagementView()
+            //Editprofileview()
+            home: AdminHomeView()
             //Editprofileview()
 
             //Licensview()
