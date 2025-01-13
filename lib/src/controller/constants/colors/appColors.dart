1d4585b6fe2d42266f842a3f70dae78ad699d887
AbuBakar Siddique <124888307+Siddique722@users.noreply.github.com>
2025-01-13 10:42:19 +0500

    up

--- lib/src/controller/constants/colors/appColors.dart
+++ lib/src/controller/constants/colors/appColors.dart
@@ -4,6 +4,7 @@ class appColors{
   static Color whiteBG=Color(0xffffffff);
   static Color secondary=Color(0xffF1510C);
   static Color grey= Color(0xff888888);
+  static Color primary= Color(0xff888888);
   static Color blacktext= Color(0xff000000);
   static Color linkBlue= Color(0xff0000ff);
 }
